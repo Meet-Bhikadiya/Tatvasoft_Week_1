439e2f46d1ee35e37e5a516cce9da45dd965c02f
Meet <bhikadiyameet88@gmail.com>
2023-03-02 22:38:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// import Counter from './Components/Counter'
+// import FunctionClick from './Components/FunctionClick'
+// import ClassClick from './Components/ClassClick'
+// import EvenBind from './Components/EventBind'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <h1>hello meet</h1>
+//       <Counter /> */}
+//       {/* <FunctionClick /> */}
+//       {/* <ClassClick /> */}
+//       <EvenBind />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import './App.css'
+import React, { useState } from 'react'
+import Home from './Pages/Home'
+import Counter from './Components/Counter'
+import About from './Components/About'
+
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home')
+  const [homeDescription, setHomeDescription] = useState('Home Page')
+  const [aboutDescription, setAboutDescription] = useState('About Page')
+  function handleButtonClick() {
+    setCurrentPage(currentPage === 'home' ? 'about' : 'home')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleButtonClick}>Toggle-Page</button>
+      {currentPage === 'home' ? (
+        <Home description={homeDescription} />
+      ) : (
+        <About description={aboutDescription} />
+      )}
+
+    
+      <h1>------</h1>
+      <Counter />
+      <h1>------</h1>
+
     </div>
   );
-}
-
-export default App;
+} 
+export default App
